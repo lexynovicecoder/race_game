9d8a215c7d1696c390df57721c7b0d5191bd2cd5
lexynovicecoder <alexandradidieye21@gmail.com>
2024-07-20 18:05:16 +0000

    new function created in functions.py to rotate car image. PlayerCar class was also created and code was written to rotate image as left and right arrow keys were pressed

--- functions.py
+++ functions.py
@@ -6,7 +6,10 @@ def scale_image(img, factor):
     return pygame.transform.scale(img, size)
     # transforms size of image according to the scale
 
-def draw(win, images):
-    for img, pos in images:
-        win.blit(img, pos)
-# function created to blit images on the screen
+
+def blit_rotate_center(win, image, top_left, angle):
+    # this rotates the image using its topleft corner but using only this can cause distortions in the image
+    rotated_image = pygame.transform.rotate(image, angle)
+    # this ensures the image only rotates in the center
+    new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
--- race_game.py
+++ race_game.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import math
-from functions import scale_image, draw
+from functions import *
 
 GRASS = scale_image(pygame.image.load("grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("track2.png"), 0.8)
@@ -9,8 +9,8 @@ TRACK = scale_image(pygame.image.load("track2.png"), 0.8)
 FINISH = pygame.image.load("finish.png")
 TRACK_BOARDER = scale_image(pygame.image.load("track2border.png"), 0.8)
 
-RED_CAR = scale_image(pygame.image.load("redcar.png"), 0.25)
-BLUE_CAR = scale_image(pygame.image.load('bluecar.png'), 0.25)
+RED_CAR = scale_image(pygame.image.load("redcar.png"), 0.17)
+BLUE_CAR = scale_image(pygame.image.load('bluecar.png'), 0.17)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 # getting width and height of track in order to use it to set the width and height of window
@@ -20,37 +20,60 @@ pygame.display.set_caption("Race Game")
 
 FPS = 60  # setting frame rate
 images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
-
-
 # making list that contains some images and their positions, so it can be used in the draw function
+
 
 class AbstractCar:  # super class to be used by both player car and computer car
     def __init__(self, max_vel, rotation_vel):  # vel meaning velocity
+        self.img = self.IMG
         self.max_vel = max_vel
         self.vel = 0  # zero because at the start your car isn't moving at all
         self.rotation_vel = rotation_vel
         self.angle = 0  # car starts at zero degrees
+        self.x, self.y = self.START_POS
     def rotate(self, left=False, right=False):
         if left:
-            self.angle -= self.rotation_vel
-        elif right:
             self.angle += self.rotation_vel
-    # this method decreases the car angle when left is True(turning left)
-    # and increases the angle when right is True(turning right)
+        elif right:
+            self.angle -= self.rotation_vel
+    # this method increases the car angle when left is True(turning left)
+    # and decreases the angle when right is True(turning right)
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x,self.y), self.angle)
+
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (732.5, 322)
+
+player_car = PlayerCar(5, 5)
+
+def draw(win, images, player_car):
+    for img, pos in images:
+        win.blit(img, pos)
+# function created to blit images on the screen
+    player_car.draw(win)
+    pygame.display.update()  # this command is essential to ensure that anything u 'draw' in pygame is updated in the
+    # display
 
 
 run = True
 clock = pygame.time.Clock()
 # trying to set the speed of the game so that the speed would be similar on anyone's computer
+
 while run:
     clock.tick(FPS)  # this ensures that loop doesn't run more than 60 frames per second
 
-    draw(WIN, images)  # calling draw function
+    draw(WIN, images, player_car)  # calling draw function
 
-    pygame.display.update()  # this command is essential to ensure that anything u 'draw' in pygame is updated in the
-    # display
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_RIGHT]:
+        player_car.rotate(right=True)
+
 pygame.quit()
