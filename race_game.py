ef48248ef07f2e4da7b097575476fca64b118174
lexynovicecoder <alexandradidieye21@gmail.com>
2024-07-24 18:13:09 +0000

    new method created to move the car forward in all directions which was done with the help of the math module and trigonometry and a method to decelerate the car when the up arrow button is released

--- race_game.py
+++ race_game.py
@@ -20,6 +20,8 @@ pygame.display.set_caption("Race Game")
 
 FPS = 60  # setting frame rate
 images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+
+
 # making list that contains some images and their positions, so it can be used in the draw function
 
 
@@ -31,26 +33,53 @@ class AbstractCar:  # super class to be used by both player car and computer car
         self.rotation_vel = rotation_vel
         self.angle = 0  # car starts at zero degrees
         self.x, self.y = self.START_POS
+        self.acceleration = 0.1
+
     def rotate(self, left=False, right=False):
         if left:
             self.angle += self.rotation_vel
         elif right:
             self.angle -= self.rotation_vel
+
     # this method increases the car angle when left is True(turning left)
     # and decreases the angle when right is True(turning right)
+
     def draw(self, win):
-        blit_rotate_center(win, self.img, (self.x,self.y), self.angle)
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        # this method says that if the car's velocity becomes greater than the maximum velocity it should move with
+        # the maximum velocity but if not it should keep accelerating
+        self.move()
+
+    def move(self):
+        radians = math.radians(self.angle)  # this is a special way used to calculate angles in computers
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        self.y -= vertical
+        self.x -= horizontal
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration, 0)
+        # this is so because if the velocity happens to become negative we don't want to move backwards hence it
+         # automatically sets to zero
+        self.move()
+
 
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
     START_POS = (732.5, 322)
 
-player_car = PlayerCar(5, 5)
+
+player_car = PlayerCar(4, 4)
+
 
 def draw(win, images, player_car):
     for img, pos in images:
         win.blit(img, pos)
-# function created to blit images on the screen
+    # function created to blit images on the screen
     player_car.draw(win)
     pygame.display.update()  # this command is essential to ensure that anything u 'draw' in pygame is updated in the
     # display
@@ -75,5 +104,9 @@ while run:
         player_car.rotate(left=True)
     if keys[pygame.K_RIGHT]:
         player_car.rotate(right=True)
+    if keys[pygame.K_UP]:
+        player_car.move_forward()
+    if not keys[pygame.K_UP]:
+        player_car.reduce_speed()
 
 pygame.quit()
