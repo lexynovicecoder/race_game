137ccce6f6d7a803dbab3efa2bcc82cc33a845f7
lexynovicecoder <alexandradidieye21@gmail.com>
2024-07-31 18:22:53 +0000

    pixel perfect collision to help detect collisions between player car and border and also the finish line

--- race_game.py
+++ race_game.py
@@ -7,7 +7,12 @@ GRASS = scale_image(pygame.image.load("grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("track2.png"), 0.8)
 
 FINISH = pygame.image.load("finish.png")
-TRACK_BOARDER = scale_image(pygame.image.load("track2border.png"), 0.8)
+FINISH_POS = (732.5, 395)
+FINISH_MASK = pygame.mask.from_surface(FINISH)
+TRACK_BORDER = scale_image(pygame.image.load("track2border.png"), 0.8)
+
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+# getting the mask of the track border to help in detecting pixel perfect collision
 
 RED_CAR = scale_image(pygame.image.load("redcar.png"), 0.17)
 BLUE_CAR = scale_image(pygame.image.load('bluecar.png'), 0.17)
@@ -18,8 +23,8 @@ WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Race Game")
 
-FPS = 60  # setting frame rate
-images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+FPS = 70  # setting frame rate
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH,(FINISH_POS)), (TRACK_BORDER, (0, 0))]
 
 
 # making list that contains some images and their positions, so it can be used in the draw function
@@ -53,6 +58,17 @@ class AbstractCar:  # super class to be used by both player car and computer car
         # the maximum velocity but if not it should keep accelerating
         self.move()
 
+    def move_backward(self):
+        self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
+        # we are halfing the negative velocity because when reversing a car you can't go as fast as when moving forward
+        self.move()
+
+    def collide(self, mask, x = 0, y = 0):
+       car_mask = pygame.mask.from_surface(self.img)
+       offset = (int(self.x - x), int(self.y - y))
+       poi = mask.overlap(car_mask, offset)
+       return poi
+
     def move(self):
         radians = math.radians(self.angle)  # this is a special way used to calculate angles in computers
         vertical = math.cos(radians) * self.vel
@@ -61,19 +77,44 @@ class AbstractCar:  # super class to be used by both player car and computer car
         self.y -= vertical
         self.x -= horizontal
 
+
+
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (732.5, 340)
+
     def reduce_speed(self):
         self.vel = max(self.vel - self.acceleration, 0)
         # this is so because if the velocity happens to become negative we don't want to move backwards hence it
          # automatically sets to zero
         self.move()
 
+    def bounce(self):
+        self.vel = -self.vel# reverse velocity so if the car hits it moving forward it goes back and vice versa
+        self.move()
 
-class PlayerCar(AbstractCar):
-    IMG = RED_CAR
-    START_POS = (732.5, 322)
 
 
 player_car = PlayerCar(4, 4)
+
+def move_player(player_car):
+    moved = False
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_RIGHT]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_UP]:
+        moved = True
+        player_car.move_forward()
+    if keys[pygame.K_DOWN]:
+        moved = True
+        player_car.move_backward()
+
+    if not moved:
+        player_car.reduce_speed()
+
 
 
 def draw(win, images, player_car):
@@ -98,15 +139,17 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             break
-    keys = pygame.key.get_pressed()
+    move_player(player_car)
 
-    if keys[pygame.K_LEFT]:
-        player_car.rotate(left=True)
-    if keys[pygame.K_RIGHT]:
-        player_car.rotate(right=True)
-    if keys[pygame.K_UP]:
-        player_car.move_forward()
-    if not keys[pygame.K_UP]:
-        player_car.reduce_speed()
+
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()
+
+    finish_poi = player_car.collide(FINISH_MASK, *FINISH_POS)
+    if finish_poi != None:
+        if finish_poi[1] == 0:
+            player_car.bounce()
+
+
 
 pygame.quit()
